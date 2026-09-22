b1497ffd5d86b700e0d54d7de8d121bdadabcddd
protlysbeverage <protlysbeverage@gmail.com>
2026-09-22 21:26:49 +0300

    Reduce mobile bottom spacing

--- components/AppShell.js
+++ components/AppShell.js
@@ -91,8 +91,8 @@ export default function AppShell({ children }) {
         height:auto!important;
         overflow-y:auto!important;
         overflow-x:hidden!important;
-        padding-bottom:180px!important;
-        scroll-padding-bottom:180px!important;
+        padding-bottom:128px!important;
+        scroll-padding-bottom:128px!important;
         -webkit-overflow-scrolling:touch!important;
       }
     }
